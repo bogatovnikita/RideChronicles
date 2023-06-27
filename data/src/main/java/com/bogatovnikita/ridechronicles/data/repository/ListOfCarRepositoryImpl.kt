feff06bab8575c3544f228a63ebbf1167c2c3187
Nikita Bogatov <bogatovnikita@gmail.com>
2023-06-27 18:41:12 +0400

    add implementation for get list car

--- data/src/main/java/com/bogatovnikita/ridechronicles/data/repository/ListOfCarRepositoryImpl.kt
+++ data/src/main/java/com/bogatovnikita/ridechronicles/data/repository/ListOfCarRepositoryImpl.kt
@@ -1,10 +1,31 @@
 package com.bogatovnikita.ridechronicles.data.repository
 
-import com.bogatovnikita.ridechronicles.domain.models.Car
+import com.bogatovnikita.ridechronicles.data.retrofit.ApiService
+import com.bogatovnikita.ridechronicles.data.retrofit.models.TransmissionName
+import com.bogatovnikita.ridechronicles.domain.models.CarForList
 import com.bogatovnikita.ridechronicles.domain.repository.ListOfCarRepository
+import retrofit2.awaitResponse
+import javax.inject.Inject
 
-class ListOfCarRepositoryImpl: ListOfCarRepository {
-    override suspend fun getListOfCar(): List<Car> {
-        TODO("Not yet implemented")
+class ListOfCarRepositoryImpl @Inject constructor(private val apiService: ApiService) :
+    ListOfCarRepository {
+
+    override suspend fun getListOfCar(page: Int): List<CarForList> {
+        val response = apiService.getListCars(page).awaitResponse()
+        return if (response.isSuccessful && response.body() !== null) {
+            response.body()!!.map {
+                CarForList(
+                    idCar = it.id,
+                    brandName = it.brandName,
+                    modelName = it.modelName,
+                    engineName = it.engineName,
+                    year = it.year,
+                    transmissionName = if (it.transmissionName == TransmissionName.At) "AT" else "MT",
+                    urlPhoto = it.image
+                )
+            }
+        } else {
+            emptyList()
+        }
     }
 }
