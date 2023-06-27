f07bc9c32f82d72855d8033ce4b9e4b54a56abf2
Nikita Bogatov <bogatovnikita@gmail.com>
2023-06-27 18:39:02 +0400

    add retrofit module for hilt

---
+++ app/src/main/java/com/bogatovnikita/ridechronicles/di/ApiModule.kt
@@ -0,0 +1,28 @@
+package com.bogatovnikita.ridechronicles.di
+
+import com.bogatovnikita.ridechronicles.data.retrofit.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    private const val BASE_URL = "http://am111.05.testing.place/api/v1/"
+
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
